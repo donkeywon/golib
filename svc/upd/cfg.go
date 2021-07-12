6290c3a5f93bf2394d0ed06559f76160b263a66e
donkeywon <donkeywon@163.com>
2021-07-12 18:52:54 +0800

    upd svc optimize

--- svc/upd/cfg.go
+++ svc/upd/cfg.go
@@ -1,20 +1,27 @@
 package upd
 
 const (
-	DefaultDownloadDir       = "/tmp"
-	DefaultDownloadRateLimit = 1048576 // 1MB/s
+	DefaultDownloadDir             = "/tmp"
+	DefaultDownloadRateLimit       = 1048576 // 1MB/s
+	DefaultExtractDir              = "golib-upgrade-svc-tmp"
+	DefaultUpgradeDeployScriptPath = "bin/upgrade_deploy.sh"
+	DefaultUpgradeStartScriptPath  = "bin/upgrade_start.sh"
 )
 
 type Cfg struct {
-	DownloadDir       string `env:"UPD_DOWNLOAD_DIR"        yaml:"downloadPath"`
-	DownloadRateLimit int    `env:"UPD_DOWNLOAD_RATE"       yaml:"downloadRateLimit"`
-	ExtractDir        string `env:"UPD_EXTRACT_DIR"         validate:"required"      yaml:"extractDir"`
-	UpgradeScriptPath string `env:"UPD_UPGRADE_SCRIPT_PATH" validate:"required"      yaml:"upgradeScriptPath"`
+	DownloadDir             string `env:"UPD_DOWNLOAD_DIR"              yaml:"downloadPath"`
+	DownloadRateLimit       int    `env:"UPD_DOWNLOAD_RATE"             yaml:"downloadRateLimit"`
+	ExtractDir              string `env:"UPD_EXTRACT_DIR"               validate:"required"            yaml:"extractDir"`       // starting with / means absolute path, otherwise means filepath.Join(DownloadDir, ExtractDir)
+	UpgradeDeployScriptPath string `env:"UPD_DEPLOY_SCRIPT_PATH"        validate:"required"            yaml:"deployScriptPath"` // starting with / means absolute path, otherwise means filepath.Join(ExtractDir, DeployScriptPath)
+	UpgradeStartScriptPath  string `env:"UPD_UPGRADE_START_SCRIPT_PATH" yaml:"upgradeStartScriptPath"`                          // not exec when empty, starting with / means absolute path, otherwise means filepath.Join(ExtractDir, UpgradeStartScriptPath)
 }
 
 func NewCfg() *Cfg {
 	return &Cfg{
-		DownloadDir:       DefaultDownloadDir,
-		DownloadRateLimit: DefaultDownloadRateLimit,
+		DownloadDir:             DefaultDownloadDir,
+		DownloadRateLimit:       DefaultDownloadRateLimit,
+		ExtractDir:              DefaultExtractDir,
+		UpgradeDeployScriptPath: DefaultUpgradeDeployScriptPath,
+		UpgradeStartScriptPath:  DefaultUpgradeStartScriptPath,
 	}
 }
