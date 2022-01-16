f625d56e081336767be1093a6449ba6e929bd0d2
donkeywon <donkeywon@163.com>
2022-01-17 00:52:21 +0800

    upd daemon

--- daemon/upd/ver.go
+++ daemon/upd/ver.go
@@ -3,7 +3,8 @@ package upd
 import "github.com/donkeywon/golib/pipeline"
 
 type VerInfo struct {
-	Filename string          `json:"filename" yaml:"filename"`
-	Ver      string          `json:"ver"      yaml:"ver"`
-	StoreCfg *pipeline.RWCfg `json:"storeCfg" yaml:"storeCfg"`
+	Ver             string          `json:"ver"             yaml:"ver"             validate:"required"`
+	StoreCfg        *pipeline.RWCfg `json:"storeCfg"        yaml:"storeCfg"        validate:"required"`
+	DownloadDstPath string          `json:"downloadDstPath" yaml:"downloadDstPath" validate:"required"`
+	DeployCmd       []string        `json:"deployCmd"       yaml:"deployCmd"       validate:"required"`
 }
