2faf902fb1a28cfc06a28ffbbae6853c29a8d5ea
donkeywon <donkeywon@163.com>
2022-03-12 19:31:43 +0800

    optimize httpio

--- oss/reader.go
+++ oss/reader.go
@@ -6,32 +6,35 @@ import (
 	"time"
 
 	"github.com/donkeywon/golib/util/httpc"
-	"github.com/donkeywon/golib/util/httpio/reader"
+	"github.com/donkeywon/golib/util/httpio"
 	"github.com/donkeywon/golib/util/oss"
 )
 
 type Reader struct {
 	*Cfg
-	*reader.Reader
+	*httpio.Reader
 }
 
-func NewReader(ctx context.Context, cfg *Cfg, opts ...reader.Option) *Reader {
+func NewReader(ctx context.Context, cfg *Cfg, opts ...httpc.Option) *Reader {
 	r := &Reader{
 		Cfg: cfg,
 	}
-	allOpts := make([]reader.Option, 0, 2+len(opts))
+	rc := &httpio.Cfg{
+		Retry:    cfg.Retry,
+		BeginPos: cfg.BeginPos,
+	}
+	allOpts := make([]httpc.Option, 0, 1+len(opts))
 	allOpts = append(allOpts,
-		reader.Retry(cfg.Retry),
-		reader.WithReqOptions(
-			httpc.ReqOptionFunc(func(r *http.Request) error {
-				return oss.Sign(r, cfg.Ak, cfg.Sk, cfg.Region)
-			}),
-		))
+		httpc.ReqOptionFunc(func(r *http.Request) error {
+			return oss.Sign(r, cfg.Ak, cfg.Sk, cfg.Region)
+		}),
+	)
 	allOpts = append(allOpts, opts...)
 
-	r.Reader = reader.New(ctx,
+	r.Reader = httpio.New(ctx,
 		time.Second*time.Duration(cfg.Timeout),
 		cfg.URL,
+		rc,
 		allOpts...,
 	)
 	return r
