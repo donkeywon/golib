d78970151dde4fe116b6ef3550f4b7f2dfc94cab
donkeywon <donkeywon@163.com>
2022-03-13 19:58:51 +0800

    save temp

--- util/httpio/cfg.go
+++
@@ -1,8 +0,0 @@
-package httpio
-
-type Cfg struct {
-	BeginPos int64 `json:"beginPos" yaml:"beginPos"`
-	EndPos   int64 `json:"endPos"   yaml:"endPos"`
-	PartSize int64 `json:"partSize" yaml:"partSize"`
-	Retry    int   `json:"retry"    yaml:"retry"`
-}
