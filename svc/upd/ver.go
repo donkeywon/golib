ae1c231d04031140dd689babbd6412e2e5a0b6f0
donkeywon <donkeywon@163.com>
2024-07-29 19:05:47 +0800

    move svc to another mod

--- svc/upd/ver.go
+++
@@ -1,9 +0,0 @@
-package upd
-
-import "github.com/donkeywon/golib/pipeline"
-
-type VerInfo struct {
-	Filename string             `json:"filename" yaml:"filename"`
-	Ver      string             `json:"ver"      yaml:"ver"`
-	StoreCfg *pipeline.StoreCfg `json:"storeCfg" yaml:"storeCfg"`
-}
