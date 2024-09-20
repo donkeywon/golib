7ec142ba939b122820dbed9c3c3d35906c82cbf1
donkeywon <donkeywon@163.com>
2024-09-20 13:22:09 +0800

    fix upd

--- daemon/upd/ver.go
+++ daemon/upd/ver.go
@@ -3,7 +3,7 @@ package upd
 import "github.com/donkeywon/golib/pipeline"
 
 type VerInfo struct {
-	Filename string             `json:"filename" yaml:"filename"`
-	Ver      string             `json:"ver"      yaml:"ver"`
-	StoreCfg *pipeline.StoreCfg `json:"storeCfg" yaml:"storeCfg"`
+	Filename string          `json:"filename" yaml:"filename"`
+	Ver      string          `json:"ver"      yaml:"ver"`
+	StoreCfg *pipeline.RWCfg `json:"storeCfg" yaml:"storeCfg"`
 }
