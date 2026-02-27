dfea15e6b66ad5f04c4135e98605abdc8ec4ea17
donkeywon <donkeywon@163.com>
2026-02-27 20:26:37 +0800

    dbp pools cfg support env and flag

--- daemon/dbp/cfg.go
+++ daemon/dbp/cfg.go
@@ -2,6 +2,8 @@ package dbp
 
 import (
 	"time"
+
+	"github.com/donkeywon/golib/util/jsons"
 )
 
 const (
@@ -9,19 +11,27 @@ const (
 )
 
 type PoolCfg struct {
-	Name             string        `yaml:"name"`
-	Type             string        `yaml:"type"`
-	DSN              string        `yaml:"dsn"`
-	MaxIdle          int           `yaml:"maxIdle"`
-	MaxOpen          int           `yaml:"maxOpen"`
-	MaxLifeTime      time.Duration `yaml:"maxLifeTime"`
-	MaxIdleTime      time.Duration `yaml:"maxIdleTime"`
-	MaxWaitReadyTime time.Duration `yaml:"maxWaitReadyTime"`
-	ReadyQuery       string        `yaml:"readyQuery"`
+	Name             string        `json:"name"                yaml:"name"`
+	Type             string        `json:"type"                yaml:"type"`
+	DSN              string        `json:"dsn"                 yaml:"dsn"`
+	MaxIdle          int           `json:"max_idle"            yaml:"maxIdle"`
+	MaxOpen          int           `json:"max_open"            yaml:"maxOpen"`
+	MaxLifeTime      time.Duration `json:"max_life_time"       yaml:"maxLifeTime"`
+	MaxIdleTime      time.Duration `json:"max_idle_time"       yaml:"maxIdleTime"`
+	MaxWaitReadyTime time.Duration `json:"max_wait_ready_time" yaml:"maxWaitReadyTime"`
+	ReadyQuery       string        `json:"ready_query"         yaml:"readyQuery"`
+}
+
+func (pc *PoolCfg) UnmarshalFlag(value string) error {
+	return jsons.UnmarshalString(value, pc)
+}
+
+func (pc PoolCfg) MarshalFlag() (string, error) {
+	return jsons.MarshalString(pc)
 }
 
 type Cfg struct {
-	Pools               []*PoolCfg `yaml:"pools"`
+	Pools               []*PoolCfg `env:"POOLS" env-delim:";"   yaml:"pools"               long:"pools"                 description:"database connection pools"                                 validate:"required"`
 	EnableExportMetrics bool       `env:"ENABLE_EXPORT_METRICS" yaml:"enableExportMetrics" long:"enable-export-metrics" description:"export database conn pool metrics with prometheus protocol"`
 }
 
