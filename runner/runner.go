aaaa6a6e3bd200f0645e2283b5bcfb80d0fff8e9
donkeywon <donkeywon@163.com>
2026-05-18 21:19:23 +0800

    optimize runner

--- runner/runner.go
+++ runner/runner.go
@@ -7,7 +7,6 @@ import (
 	"sync"
 
 	"github.com/donkeywon/golib/errs"
-	"github.com/donkeywon/golib/logs"
 )
 
 type Lifecycle interface {
@@ -17,33 +16,32 @@ type Lifecycle interface {
 }
 
 type Signaler interface {
-	MarkInitialized() bool
-	MarkStarted() bool
-	MarkStopping() bool
-	MarkStopDone() bool
-	MarkDone() bool
-	Initialized() <-chan struct{}
 	Started() <-chan struct{}
 	Stopping() <-chan struct{}
 	StopDone() <-chan struct{}
 	Done() <-chan struct{}
 }
 
+type marker interface {
+	markStarted() bool
+	markStopping() bool
+	markStopDone() bool
+	markDone() bool
+}
+
 type Runner interface {
 	Lifecycle
 	Signaler
+
+	marker
 }
 
-// Init a runner.
 func Init(ctx context.Context, r Runner) (err error) {
 	if r == nil {
 		panic("nil runner")
 	}
 	if ctx == nil {
 		panic("nil context")
-	}
-	if !r.MarkInitialized() {
-		panic("init again")
 	}
 
 	defer func() {
@@ -58,17 +56,9 @@ func Init(ctx context.Context, r Runner) (err error) {
 		}
 	}()
 
-	l := logs.FromCtx(ctx)
-	l.Info("init")
-	err = r.Init(ctx)
-	if err != nil {
-		return
-	}
-	l.Info("init done")
-	return
+	return r.Init(ctx)
 }
 
-// Run a runner and wait it done.
 func Start(ctx context.Context, r Runner) (err error) {
 	if r == nil {
 		panic("nil runner")
@@ -78,19 +68,9 @@ func Start(ctx context.Context, r Runner) (err error) {
 	}
 
 	select {
-	case <-r.Initialized():
-	default:
-		panic("start before initialized")
-	}
-
-	select {
 	case <-r.Stopping():
 		panic("start after stopping")
 	default:
-	}
-
-	if !r.MarkStarted() {
-		panic("run again")
 	}
 
 	select {
@@ -99,11 +79,14 @@ func Start(ctx context.Context, r Runner) (err error) {
 	default:
 	}
 
-	l := logs.FromCtx(ctx)
+	if !r.markStarted() {
+		panic("start again")
+	}
+
 	defer func() {
 		p := recover()
 		if p != nil {
-			pe := errs.PanicToErrWithMsg(p, fmt.Sprintf("panic on run runner: %T", r))
+			pe := errs.PanicToErrWithMsg(p, fmt.Sprintf("panic on start runner: %T", r))
 			if err == nil {
 				err = pe
 			} else {
@@ -111,56 +94,61 @@ func Start(ctx context.Context, r Runner) (err error) {
 			}
 		}
 
-		l.Info("done")
-		r.MarkDone()
+		r.markDone()
 	}()
 
 	go func() {
 		select {
 		case <-r.Stopping():
 			return
+		case <-r.Done():
+			return
 		case <-ctx.Done():
 			select {
 			case <-r.Stopping():
 				return
+			case <-r.Done():
+				return
 			default:
 			}
-			l.Info("canceling")
-			stop(context.Background(), r, false)
+			stop(context.WithoutCancel(ctx), r, false)
 		}
 	}()
 
-	l.Info("starting")
 	return r.Start(ctx)
 }
 
-// Stop runner, in most scenario, Stop is notification action to notify the Runner to stop.
 func Stop(ctx context.Context, r Runner) error {
 	return stop(ctx, r, false)
 }
 
-// StopAndWait notify Runner to stop and wait it done.
 func StopAndWait(ctx context.Context, r Runner) error {
 	return stop(ctx, r, true)
 }
 
 func stop(ctx context.Context, r Runner, wait bool) (err error) {
+	if ctx == nil {
+		panic("nil context")
+	}
+	if r == nil {
+		panic("nil runner")
+	}
+
 	select {
 	case <-r.Started():
 	default:
 		panic("stop before start")
 	}
 
-	l := logs.FromCtx(ctx)
-	if !r.MarkStopping() {
-		l.Info("already stopping", "wait", wait)
-		return waitDone(ctx, r)
+	if !r.markStopping() {
+		if wait {
+			return waitDone(ctx, r)
+		}
+		return nil
 	}
 
-	l.Info("stopping", "wait", wait)
 	err = safeStop(ctx, r)
-	l.Info("stop done")
-	r.MarkStopDone()
+	r.markStopDone()
 	if wait {
 		waitErr := waitDone(ctx, r)
 		if waitErr != nil {
@@ -197,22 +185,19 @@ func waitDone(ctx context.Context, r Runner) error {
 }
 
 type Base struct {
-	initialized     chan struct{}
-	started         chan struct{}
-	stopping        chan struct{}
-	done            chan struct{}
-	stopDone        chan struct{}
-	initializedOnce sync.Once
-	startedOnce     sync.Once
-	stoppingOnce    sync.Once
-	doneOnce        sync.Once
-	stopDoneOnce    sync.Once
-	initOnce        sync.Once
+	started      chan struct{}
+	stopping     chan struct{}
+	done         chan struct{}
+	stopDone     chan struct{}
+	startedOnce  sync.Once
+	stoppingOnce sync.Once
+	doneOnce     sync.Once
+	stopDoneOnce sync.Once
+	initOnce     sync.Once
 }
 
 func (br *Base) init() {
 	br.initOnce.Do(func() {
-		br.initialized = make(chan struct{})
 		br.started = make(chan struct{})
 		br.stopping = make(chan struct{})
 		br.done = make(chan struct{})
@@ -225,20 +210,8 @@ func (br *Base) Init(_ context.Context) error {
 	return nil
 }
 
-func (br *Base) Start(_ context.Context) error {
-	br.init()
-	<-br.Stopping()
-	return nil
-}
-
 func (br *Base) Stop(_ context.Context) error {
-	br.init()
 	return nil
-}
-
-func (br *Base) Initialized() <-chan struct{} {
-	br.init()
-	return br.initialized
 }
 
 func (br *Base) Started() <-chan struct{} {
@@ -261,28 +234,27 @@ func (br *Base) Done() <-chan struct{} {
 	return br.done
 }
 
-func (br *Base) MarkInitialized() bool {
-	return br.closeCh(&br.initializedOnce, br.initialized)
-}
-
-func (br *Base) MarkStarted() bool {
+func (br *Base) markStarted() bool {
+	br.init()
 	return br.closeCh(&br.startedOnce, br.started)
 }
 
-func (br *Base) MarkStopping() bool {
+func (br *Base) markStopping() bool {
+	br.init()
 	return br.closeCh(&br.stoppingOnce, br.stopping)
 }
 
-func (br *Base) MarkStopDone() bool {
+func (br *Base) markStopDone() bool {
+	br.init()
 	return br.closeCh(&br.stopDoneOnce, br.stopDone)
 }
 
-func (br *Base) MarkDone() bool {
+func (br *Base) markDone() bool {
+	br.init()
 	return br.closeCh(&br.doneOnce, br.done)
 }
 
 func (br *Base) closeCh(once *sync.Once, ch chan struct{}) bool {
-	br.init()
 	closed := false
 	once.Do(func() {
 		close(ch)
